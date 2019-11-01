9d6a445d32da4a5bd6ec7c184c9a22463c120774
Thomas Donovan <tdonovan79@gmail.com>
2019-11-01 22:59:21 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -22,12 +22,14 @@ def assembled_matrix
   residents = [["Don Gately",  "Joelle van Dyne",  "Pat M.", "Kate Gompert",  "Bruce Green"],
   ["Hal Incandenza",  "Lyle",  "Gerhard Schtitt",  "Mario Incandenza",  "Michael Pemulis"]
   ]
+  return residents
 end
 
 def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
+  
 end
 
 def matrix_lookup(matrix, row, column)
